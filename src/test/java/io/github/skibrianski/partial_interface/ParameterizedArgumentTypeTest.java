b5e9918063d2c4af5a1a93320597a7456cf24086
Brian Szymanski <bszymanski@cvent.com>
2024-01-20 15:04:15 -0500

    add failing test so i don't forget

---
+++ src/test/java/io/github/skibrianski/partial_interface/ParameterizedArgumentTypeTest.java
@@ -0,0 +1,14 @@
+package io.github.skibrianski.partial_interface;
+
+import io.github.skibrianski.partial_interface.exception.PartialInterfaceNotCompletedException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ParameterizedArgumentTypeTest {
+
+    @Test
+    void test_TODO() {
+        throw new RuntimeException("not implemented");
+    }
+
+}
