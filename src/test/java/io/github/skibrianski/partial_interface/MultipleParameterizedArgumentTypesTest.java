cc5ba878349346a096de082b7ef0c6ee04591c8f
Brian Szymanski <bszymanski@cvent.com>
2024-01-20 17:59:26 -0500

    full tests for multiple parameterized argument types

--- src/test/java/io/github/skibrianski/partial_interface/MultipleParameterizedArgumentTypesTest.java
+++ src/test/java/io/github/skibrianski/partial_interface/MultipleParameterizedArgumentTypesTest.java
@@ -1,5 +1,6 @@
 package io.github.skibrianski.partial_interface;
 
+import io.github.skibrianski.partial_interface.exception.PartialInterfaceNotCompletedException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -27,8 +28,58 @@ public class MultipleParameterizedArgumentTypesTest {
         public void method(int foo, String bar) { }
     }
     @Test
-    void test_happyPath() {
+    void test_valid_happyPath() {
         Assertions.assertDoesNotThrow(() -> PartialInterface.check(Valid.class));
+    }
+
+    public static class A { }
+    public static class AChild extends A { }
+    public static class B { }
+    public static class BChild extends B { }
+
+    @HasTypeParameters({A.class, B.class})
+    static class ChildReturnTypes implements HasMethodWithMultipleTypeParameters {
+        public void method(AChild foo, BChild bar) { }
+    }
+    @Test
+    void test_valid_childReturnType() {
+        Assertions.assertDoesNotThrow(() -> PartialInterface.check(Valid.class));
+    }
+
+    @HasTypeParameters({int.class, String.class})
+    static class FirstParameterIsIncorrect implements HasMethodWithMultipleTypeParameters {
+        public void method(String String, int bar) { }
+    }
+    @Test
+    void test_invalid_firstParameterIsIncorrect() {
+        Assertions.assertThrows(
+                PartialInterfaceNotCompletedException.class,
+                () -> PartialInterface.check(FirstParameterIsIncorrect.class)
+        );
+    }
+
+    @HasTypeParameters({int.class, String.class})
+    static class SecondParameterIsIncorrect implements HasMethodWithMultipleTypeParameters {
+        public void method(int foo, int bar) { }
+    }
+    @Test
+    void test_invalid_secondParameterIsIncorrect() {
+        Assertions.assertThrows(
+                PartialInterfaceNotCompletedException.class,
+                () -> PartialInterface.check(SecondParameterIsIncorrect.class)
+        );
+    }
+
+    @HasTypeParameters({int.class, String.class})
+    static class ExtraParameter implements HasMethodWithMultipleTypeParameters {
+        public void method(int foo, String bar, boolean extra) { }
+    }
+    @Test
+    void test_invalid_extraParameter() {
+        Assertions.assertThrows(
+                PartialInterfaceNotCompletedException.class,
+                () -> PartialInterface.check(ExtraParameter.class)
+        );
     }
 
 }
