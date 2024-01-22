20f39faed3f7a2fbf09a3b8e3158c34098825cd0
Brian Szymanski <bszymanski@cvent.com>
2024-01-22 13:56:50 -0500

    WIP: typeString parsing

--- src/main/java/io/github/skibrianski/partial_interface/internal/IType.java
+++ src/main/java/io/github/skibrianski/partial_interface/internal/IType.java
@@ -1,6 +1,8 @@
 package io.github.skibrianski.partial_interface.internal;
 
 import io.github.skibrianski.partial_interface.Type;
+import io.github.skibrianski.partial_interface.exception.PartialInterfaceNotCompletedException;
+import io.github.skibrianski.partial_interface.exception.PartialInterfaceUsageException;
 
 import java.util.Map;
 
@@ -21,18 +23,55 @@ public abstract class IType {
     public abstract Class<?> getActualType();
 
     public static IType convertFromAnnotation(Type type, Map<String, Class<?>> typeParameterMap) {
-        if (!type.byClass().equals(Type.NotSpecified.class)) {
-            return new ClassType<>(type.byClass(), typeParameterMap);
+        return parse(type.byClass(), type.value(), typeParameterMap);
+    }
+
+    public static IType parse(
+            Class<?> typeClass,
+            String typeString,
+            Map<String, Class<?>> typeParameterMap
+    ) {
+        if (!typeClass.equals(Type.NotSpecified.class)) {
+            return new ClassType<>(typeClass, typeParameterMap);
         }
 
-        String typeString = type.value();
-        if (!typeString.contains("<")) {
+        int nextOpen = typeString.indexOf('<');
+        if (nextOpen == -1) {
             return new TypeVariable(typeString, typeParameterMap);
         }
 
-        if (typeString.matches("^[^<]+<[^>]+>$")) {
-            throw new RuntimeException("unimplemented - match");
+        String variable = typeString.substring(0, nextOpen);
+        String typeParameterArgumentsString = typeString.substring(
+                nextOpen + 1,
+                typeString.lastIndexOf('>')
+        );
+        // if input was: `Map<R, X<String>>`, variable will be `Map` and typeParameterArgumentsString `R, X<String>`
+        try {
+            Class<?> baseClass = Class.forName(variable);
+            // TODO: we now have `Foo, Map<UUID, Bar>, Baz` or something. call parseList(typeParameterArgumentsString)
+//            return new ParameterizedType(baseClass, ...);
+        } catch (ClassNotFoundException e) {
+            throw new PartialInterfaceUsageException(
+                    "cannot load class: " + variable + ", try a fully qualified type like java.util.List instead"
+            );
         }
-        throw new RuntimeException("unimplemented");
+        throw new RuntimeException("not implemented");
+    }
+
+    public static IType[] parseList(String typeParameterArgumentString, Map<String, Class<?>> typeParameterMap) {
+        int nextComma = notFoundIsMaxInt(typeParameterArgumentString.indexOf(','));
+        int nextClose = notFoundIsMaxInt(typeParameterArgumentString.indexOf('>'));
+        if (nextComma < nextClose) {
+            String typeParameterString = typeParameterArgumentString.substring(0, nextComma);
+            // there's another arg
+        } else {
+            String typeParameterString = typeParameterArgumentString.substring(0, nextClose);
+            // this is the last arg
+        }
+        throw new RuntimeException("not implemented");
+    }
+
+    private static int notFoundIsMaxInt(int input) {
+        return input == -1 ? Integer.MAX_VALUE : input;
     }
 }
