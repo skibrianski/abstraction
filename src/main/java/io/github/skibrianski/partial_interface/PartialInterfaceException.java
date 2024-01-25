bede076a1fefa685f00c150f1065c4a57599ede1
Brian Szymanski <bszymanski@cvent.com>
2024-01-24 23:27:00 -0500

    remove a todo that is done

--- src/main/java/io/github/skibrianski/partial_interface/PartialInterfaceException.java
+++ src/main/java/io/github/skibrianski/partial_interface/PartialInterfaceException.java
@@ -1,6 +1,5 @@
 package io.github.skibrianski.partial_interface;
 
-// TODO: nest subclasses in here.
 public class PartialInterfaceException extends RuntimeException {
     public PartialInterfaceException(String s) {
         super(s);
