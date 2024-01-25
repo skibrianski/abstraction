4722cbd33b31f2144fc1cb09569e525c53675e9a
Brian Szymanski <bszymanski@cvent.com>
2024-01-24 19:23:23 -0500

    rename

--- src/test/java/io/github/skibrianski/partial_interface/CompoundTypeArgumentTest.java
+++ src/test/java/io/github/skibrianski/partial_interface/CompoundTypeArgumentTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Collection;
 import java.util.List;
 
-public class SingleParameterizedArgumentTypeTest {
+public class CompoundTypeArgumentTest {
 
 
     @RequiresChildMethod(
