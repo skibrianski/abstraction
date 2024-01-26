a3b0d19581717a013790bbeaaaa30bc497729c13
Brian Szymanski <ski@laptop>
2024-01-26 17:41:45 -0500

    WIP: restore/fix more tests

--- src/test/java/io/github/skibrianski/partial_interface/PrimitiveTest.java
+++ src/test/java/io/github/skibrianski/partial_interface/PrimitiveTest.java
@@ -1,35 +1,49 @@
-//package io.github.skibrianski.partial_interface;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//public class PrimitiveTest {
-//
-//    @RequiresChildMethod(
-//            returnType = @Type(ofClass = int.class),
-//            argumentTypes = {@Type(ofClass = int.class)},
-//            methodName = "triple"
-//    )
-//    interface WithTriple { }
-//
-//    @PartialInterfaceWithManualValidation
-//    public static class Valid implements WithTriple {
-//        public int triple(int input) {
-//            return input * 3;
-//        }
-//    }
-//    @Test
-//    void test_valid() {
-//        Assertions.assertDoesNotThrow(() -> PartialInterface.check(Valid.class));
-//    }
-//
-//    @PartialInterfaceWithManualValidation
-//    public static class Invalid implements WithTriple { }
-//    @Test
-//    void test_invalid() {
-//        Assertions.assertThrows(
-//                PartialInterfaceException.NotCompletedException.class,
-//                () -> PartialInterface.check(Invalid.class)
-//        );
-//    }
-//}
+package io.github.skibrianski.partial_interface;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PrimitiveTest {
+
+    @RequiresChildMethod(
+            returnType = @Type(ofClass = int.class),
+            argumentTypes = {@Type(ofClass = int.class)},
+            methodName = "triple"
+    )
+    interface WithTriple { }
+
+    @PartialInterfaceWithManualValidation
+    public static class Valid implements WithTriple {
+        public int triple(int input) {
+            return input * 3;
+        }
+    }
+    @Test
+    void test_valid() {
+        Assertions.assertDoesNotThrow(() -> PartialInterface.check(Valid.class));
+    }
+
+    @PartialInterfaceWithManualValidation
+    public static class InvalidWrongReturnType implements WithTriple {
+        public String triple(int input) {
+            return "bob";
+        }
+    }
+    @Test
+    void test_invalid_wrongReturnType() {
+        Assertions.assertThrows(
+                PartialInterfaceException.ClashingReturnTypeException.class,
+                () -> PartialInterface.check(InvalidWrongReturnType.class)
+        );
+    }
+
+    @PartialInterfaceWithManualValidation
+    public static class InvalidEmpty implements WithTriple { }
+    @Test
+    void test_invalid_missingMethod() {
+        Assertions.assertThrows(
+                PartialInterfaceException.NotCompletedException.class,
+                () -> PartialInterface.check(InvalidEmpty.class)
+        );
+    }
+}
