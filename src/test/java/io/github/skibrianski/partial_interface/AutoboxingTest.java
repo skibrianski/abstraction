0d4f1045c7e9b94aec4b1236557f8d8035f584b2
Brian Szymanski <ski@laptop>
2024-01-26 17:57:53 -0500

    re-enable, fix autoboxing test

--- src/test/java/io/github/skibrianski/partial_interface/AutoboxingTest.java
+++ src/test/java/io/github/skibrianski/partial_interface/AutoboxingTest.java
@@ -1,41 +1,45 @@
-//package io.github.skibrianski.partial_interface;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.List;
-//
-//public class AutoboxingTest {
-//
-//    @RequiresTypeParameter("T")
-//    @RequiresChildMethod(
-//            returnType = @Type("T"),
-//            argumentTypes = {@Type("List<T>")},
-//            methodName = "sum"
-//    )
-//    interface WithScrambler { }
-//
-//    @PartialInterfaceWithManualValidation
-//    @HasTypeParameter(name = "T", ofClass = int.class)
-//    interface Valid extends WithScrambler {
-//        int sum(List<Integer> input);
-//    }
-//    @Test
-//    void test_valid() {
-//        Assertions.assertDoesNotThrow(() -> PartialInterface.check(Valid.class));
-//    }
-//
-//    @PartialInterfaceWithManualValidation
-//    @HasTypeParameter(name = "T", ofClass = double.class)
-//    interface Invalid extends WithScrambler {
-//        int sum(List<Integer> input);
-//    }
-//    @Test
-//    void test_invalid() {
-//        Assertions.assertThrows(
-//                PartialInterfaceException.NotCompletedException.class,
-//                () -> PartialInterface.check(Invalid.class)
-//        );
-//    }
-//
-//}
+package io.github.skibrianski.partial_interface;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class AutoboxingTest {
+
+    @RequiresTypeParameter("T")
+    @RequiresChildMethod(
+            returnType = @Type("T"),
+            argumentTypes = {@Type("List<T>")},
+            methodName = "sum"
+    )
+    interface WithSum { }
+
+    @PartialInterfaceWithManualValidation
+    @HasTypeParameter(name = "T", ofClass = int.class)
+    public static class Valid implements WithSum {
+        public int sum(List<Integer> input) {
+            return input.stream().reduce(0, (a, b) -> a + b);
+        }
+    }
+    @Test
+    void test_valid() {
+        Assertions.assertDoesNotThrow(() -> PartialInterface.check(Valid.class));
+    }
+
+    @PartialInterfaceWithManualValidation
+    @HasTypeParameter(name = "T", ofClass = double.class)
+    public static class Invalid implements WithSum {
+        public int sum(List<Integer> input) {
+            return input.stream().reduce(0, (a, b) -> a + b);
+        }
+    }
+    @Test
+    void test_invalid_wrongTypes() {
+        Assertions.assertThrows(
+                PartialInterfaceException.NotCompletedException.class,
+                () -> PartialInterface.check(Invalid.class)
+        );
+    }
+
+}
