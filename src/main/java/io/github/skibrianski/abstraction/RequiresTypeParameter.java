4d7c3abf2299fe9373db9026415b820d1b328c47
Brian Szymanski <ski@laptop>
2024-01-28 22:47:17 -0500

    remove some todos that are done

--- src/main/java/io/github/skibrianski/abstraction/RequiresTypeParameter.java
+++ src/main/java/io/github/skibrianski/abstraction/RequiresTypeParameter.java
@@ -7,8 +7,6 @@ import java.lang.annotation.Target;
 @Repeatable(RequiresTypeParameters.class)
 @Target({ElementType.TYPE})
 public @interface RequiresTypeParameter {
-    // TODO: how to express type bounds? just string?
-    // TODO: can't upperBound and lowerBound both be arrays?
     String value();
     String[] upperBound() default {};
     String[] lowerBound() default {};
