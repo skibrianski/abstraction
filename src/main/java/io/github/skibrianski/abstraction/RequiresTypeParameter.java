b2887c19b684166119a270c71d8799b7d8282003
Brian Szymanski <ski@laptop>
2024-01-31 22:03:20 -0500

    update a todo

--- src/main/java/io/github/skibrianski/abstraction/RequiresTypeParameter.java
+++ src/main/java/io/github/skibrianski/abstraction/RequiresTypeParameter.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 @Repeatable(RequiresTypeParameters.class)
 @Target({ElementType.TYPE})
 public @interface RequiresTypeParameter {
-    // TODO: consider accepting this all as a String value and just parsing it out instead?
+    // TODO: this feels clunky. consider accepting this all as a String value and just parsing it out instead?
     String value();
     String[] superOf() default {};
     String[] extending() default {};
